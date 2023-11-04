886a19a9c3c0914a33859864dd3b5e826d342d3f
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-04 19:17:53 +0100

    Implement country data display

--- src/Components/UI/Container.jsx
+++ src/Components/UI/Container.jsx
@@ -1,7 +1,5 @@
 function Container(props) {
-  return (
-    <section className="bg-green-400 p-5 space-y-3">{props.children}</section>
-  );
+  return <section className="p-5 space-y-12">{props.children}</section>;
 }
 
 export default Container;
--- src/Components/UI/CountryCard.jsx
+++ src/Components/UI/CountryCard.jsx
@@ -1,19 +1,22 @@
-function CountryCard() {
+function CountryCard({ data }) {
   return (
-    <div className="flex  flex-col shadow-lg h-15">
+    <div className="flex  flex-col shadow-lg h-15 bg-white rounded-lg overflow-hidden">
       <section className="h-40">
-        <img src="" alt="flag" />
+        <img src={data.flags.png} alt="flag" className="w-full  h-full" />
       </section>
       <section className="px-10 pt-3 pb-10">
-        <p className="font-bold">United states of America</p>
+        <p className="font-bold">{data.name.common}</p>
+        {data.flag}
         <p>
-          <span className="font-bold">Population: </span>81,770,900
+          <span className="font-bold">Population: </span>
+          {data.population}
         </p>
         <p>
-          <span className="font-bold">Region: </span>Europe
+          <span className="font-bold">Region: </span>
+          {data.region}
         </p>
         <p>
-          <span className="font-bold">Capital: </span> Washington, DC.
+          <span className="font-bold">Capital: </span> {data.capital}
         </p>
       </section>
     </div>
