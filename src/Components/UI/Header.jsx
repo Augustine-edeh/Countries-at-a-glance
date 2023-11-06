fa5405b5816406803bd3f4967003abff125c9f18
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-06 22:28:21 +0100

    Remove dead style

--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex justify-between items-center shadow-md py-6 px-5 md:px-10 xl:px-40 2xl:px-72">
-      <h1 className="bg-blue-500  font-bold text-lg">Where in the world?</h1>
+      <h1 className="font-bold text-lg">Where in the world?</h1>
       <div className="flex space-x-5">
         <img src="vite.svg" alt="toggletheme" className="" />
         <p>Dark mode</p>
