71ec8d4959f981d996786dd00317106e4bae7210
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-10 02:06:22 +0100

    Modularize 'CountryNotFound UI' Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Container from "./Components/UI/Container";
 import Footer from "./Components/UI/Footer";
 import ErrorPage from "./Components/UI/FetchError";
 import LoadingUI from "./Components/UI/LoadingUI";
+import CountryNotFoundUI from "./Components/UI/CountryNotFoundUI";
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -88,11 +89,7 @@ function App() {
                   />
                 ))
             ) : (
-              <div className="my-14 md:my-20 xl:my-24 text-lightText dark:text-darkText_LightElement bg-darkText_LightElement dark:bg-darkElement shadow-lg px-5 py-3 rounded-xl">
-                <p className="font-bold text-red-500 dark:text-red-400 text-lg md:text-xl 2xl:text-2xl border-l border-r  border-gray-400 dark:border-gray-600 px-5 rounded-3xl">
-                  Country not found
-                </p>
-              </div>
+              <CountryNotFoundUI />
             )
           ) : (
             <>{isFetchError ? <ErrorPage /> : <LoadingUI />}</>
---
+++ src/Components/UI/CountryNotFoundUI.jsx
@@ -0,0 +1,11 @@
+function CountryNotFoundUI() {
+  return (
+    <div className="my-14 md:my-20 xl:my-24 text-lightText dark:text-darkText_LightElement bg-darkText_LightElement dark:bg-darkElement shadow-lg px-5 py-3 rounded-xl">
+      <p className="font-bold text-red-500 dark:text-red-400 text-lg md:text-xl 2xl:text-2xl border-l border-r  border-gray-400 dark:border-gray-600 px-5 rounded-3xl">
+        Country not found
+      </p>
+    </div>
+  );
+}
+
+export default CountryNotFoundUI;
