a431ab7ddcae9a89fab8bca613014473af3643f0
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-10 01:12:30 +0100

    feat('Country not found UI'): style 'Country not found' UI

--- src/App.jsx
+++ src/App.jsx
@@ -84,25 +84,6 @@ function App() {
         <Container
           styleClasses={"flex flex-wrap justify-around gap-5 lg:gap-10 mb-40"}
         >
-          {/* {countries && result.length > 0 ? (
-            result
-              .sort((a, b) =>
-                a.name.common
-                  .toUpperCase()
-                  .localeCompare(b.name.common.toUpperCase())
-              )
-              .map((country) => (
-                <CountryCard
-                  countryData={country}
-                  key={Math.random().toString()}
-                />
-              ))
-          ) : isFetchError ? (
-            <p>Error: {errorMessage}</p>
-          ) : (
-            <p>Country not found</p>
-          )} */}
-
           {countries.length > 0 ? (
             isFetchError ? (
               <p>Error: {errorMessage}</p>
@@ -120,7 +101,11 @@ function App() {
                   />
                 ))
             ) : (
-              <p>Country not found</p>
+              <div className="my-14 md:my-20 xl:my-24 text-lightText dark:text-darkText_LightElement bg-darkText_LightElement dark:bg-darkElement shadow-lg px-5 py-3 rounded-xl">
+                <p className="font-bold text-red-500 dark:text-red-400 text-lg md:text-xl 2xl:text-2xl border-l border-r  border-gray-400 dark:border-gray-600 px-5 rounded-3xl">
+                  Country not found
+                </p>
+              </div>
             )
           ) : (
             <p>{isFetchError ? <ErrorPage /> : "Loading"}</p>
