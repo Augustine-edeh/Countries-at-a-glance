ce4eeaabe420909df005308db55b91da2c75b5d6
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-21 21:21:03 +0100

    chore(Card): align text to left of container-section

--- src/Components/UI/Card.jsx
+++ src/Components/UI/Card.jsx
@@ -15,18 +15,21 @@ const Card = ({ countryData }) => {
       <section className="h-40 w-full">
         <img src={countryData.flags.png} alt="flag" className="w-full h-full" />
       </section>
-      <section className="px-5  pb-10">
-        <p className="font-bold mt-8 mb-5">{countryData.name.official}</p>
-        <p>
-          <span className="font-semibold">Population: </span>
-          {countryData.population}
+      <section className="px-5  pb-10 text-left">
+        <p className="font-black text-lg mt-8 mb-5">
+          {countryData.name.official}
         </p>
         <p>
-          <span className="font-semibold">Region: </span>
-          {countryData.region}
+          <span className="font-normal">Population: </span>
+          <span className="font-thin">{countryData.population}</span>
         </p>
         <p>
-          <span className="font-semibold">Capital: </span> {countryData.capital}
+          <span className="font-normal">Region: </span>
+          <span className="font-thin">{countryData.region}</span>
+        </p>
+        <p>
+          <span className="font-normal">Capital: </span>{" "}
+          <span className="font-thin">{countryData.capital}</span>
         </p>
       </section>
     </button>
