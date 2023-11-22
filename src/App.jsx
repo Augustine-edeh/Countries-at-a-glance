5f9ccee3786fd3ae126e1a7baf171953fc2148b2
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-22 12:27:28 +0100

    chore: move alert message to 'App' component
    - The underDevelopment alert message now only comes up once (when the window is loaded).

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/UI/Header";
 import Home from "./Components/UI/Home";
@@ -5,6 +6,11 @@ import Footer from "./Components/UI/Footer";
 import Country from "./Components/UI/Country";
 
 const App = () => {
+  useEffect(() => {
+    const underConstructionNote =
+      " 🚧 Project Under Construction 🚧\n\nThanks for stopping by! This project is currently under development. I'm crafting an amazing responsive experience for the best adventure, just for you.\n \n-Augustine 🚀📱 ";
+    window.onload = () => alert(underConstructionNote);
+  });
   return (
     <section className="min-h-screen bg-lightBg dark:bg-darkBg relative">
       <Header />
